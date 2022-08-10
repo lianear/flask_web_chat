7d88b43cd191d83462030a7058e65537d2092ac8
liam-neary <lin19@pitt.edu>
2022-08-10 03:18:44 -0400

    Fixed Form Routing, Implemented User DB and test query

--- chat_app.py
+++ chat_app.py
@@ -10,12 +10,16 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Chat(db.Model):
+    __bind_key__ = 'chat'
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route("/")
@@ -31,15 +35,34 @@ def login_controller():
 
 @app.route("/register/", methods=["GET", "POST"])
 def register_controller():
+    app.logger.info(f"Register Route {request.method}")
     if request.method == 'POST':
-        print(request)
-        return render_template("register.html")
+
+        r_username = request.form['register_user']
+        r_email = request.form['register_email']
+        r_pw_1 = request.form['register_pw_1']
+        r_pw_2 = request.form['register_pw_2']
+        
+        if r_pw_1 != r_pw_2:
+            app.logger.error("Register Failed: Passwords Did Not Match!")
+            return render_template("register.html")
+        
+        new_user = User(username = r_username, email = r_email, password = r_pw_1)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(f'/profile/{r_username}')
+        except Exception as e:
+            app.logger.error("Could Not Add User To Database")
+            app.logger.error(e)
+            return render_template("register.html")
+
     else:
         return render_template("register.html")
 
 @app.route("/profile/<username>")
 def profile(username=None):
-    pass
+    return render_template("chat_page.html")
 
 @app.route("/logout/")
 def unlogger():
@@ -54,4 +77,5 @@ def messages():
     pass
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
